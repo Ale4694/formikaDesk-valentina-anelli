b2f552f84a8a7dea4cfecc9cf2ac93d5050ca210
Alessandro <alessandroformica6@gmail.com>
2026-06-11 17:32:21 +0200

    fix: migration 004 aggiunge scadenza_pagamento/giorni_pagamento, gitattributes LF

--- src-tauri/migrations/004_pagamenti.sql
+++ src-tauri/migrations/004_pagamenti.sql
@@ -1,3 +1,5 @@
+ALTER TABLE documenti ADD COLUMN scadenza_pagamento TEXT;
+ALTER TABLE documenti ADD COLUMN giorni_pagamento INTEGER DEFAULT 30;
 ALTER TABLE documenti ADD COLUMN data_pagamento TEXT;
 ALTER TABLE documenti ADD COLUMN metodo_pagamento TEXT;
 ALTER TABLE documenti ADD COLUMN riferimento_pagamento TEXT;
